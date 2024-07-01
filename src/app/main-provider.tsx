10f92f35953708d70837dbc9288288e7ebd346c3
JaviPSanchez <javier.palomino-sanchez@outlook.com>
2024-07-01 11:35:28 +0200

    Remove Mantine

--- src/app/main-provider.tsx
+++ src/app/main-provider.tsx
@@ -1,11 +1,8 @@
-import { MantineProvider } from '@mantine/core';
 import * as React from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { HelmetProvider } from 'react-helmet-async';
 
 import { MainErrorFallback } from '@/components/errors/main';
-
-import '@mantine/core/styles.css';
 
 type AppProviderProps = {
   children: React.ReactNode;
@@ -13,10 +10,8 @@ type AppProviderProps = {
 
 export const AppProvider = ({ children }: AppProviderProps) => {
   return (
-    <MantineProvider>
-      <ErrorBoundary FallbackComponent={MainErrorFallback}>
-        <HelmetProvider>{children}</HelmetProvider>
-      </ErrorBoundary>
-    </MantineProvider>
+    <ErrorBoundary FallbackComponent={MainErrorFallback}>
+      <HelmetProvider>{children}</HelmetProvider>
+    </ErrorBoundary>
   );
 };
--- src/app/routes/app/root.tsx
+++ src/app/routes/app/root.tsx
@@ -1,18 +1,18 @@
 import { ErrorBoundary } from 'react-error-boundary';
 import { Outlet, useLocation } from 'react-router-dom';
 
-import { DashboardLayoutV2 } from '@/components/layouts';
+import { DashboardLayout } from '@/components/layouts';
 
 export const AppRoot = () => {
   const location = useLocation();
   return (
-    <DashboardLayoutV2>
+    <DashboardLayout>
       <ErrorBoundary
         key={location.pathname}
         fallback={<div>Something went wrong!</div>}
       >
         <Outlet />
       </ErrorBoundary>
-    </DashboardLayoutV2>
+    </DashboardLayout>
   );
 };
