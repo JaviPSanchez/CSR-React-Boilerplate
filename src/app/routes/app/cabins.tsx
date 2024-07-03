f9c0c94f0e67fd84bdb10bb337a461e21d25e9fc
JaviPSanchez <javier.palomino-sanchez@outlook.com>
2024-07-03 16:10:18 +0200

    Connect to backend

--- src/app/routes/app/cabins.tsx
+++ src/app/routes/app/cabins.tsx
@@ -1,11 +1,21 @@
+import { useEffect } from 'react';
+
 import { Heading } from '@/components/ui/heading';
 import { Row } from '@/components/ui/row';
+import { getCabins } from '@/services/api-cabins';
 
 export const Cabins = () => {
+  useEffect(function () {
+    getCabins().then(data => console.log(data));
+  }, []);
   return (
     <Row type="horizontal">
       <Heading type="h1" title="All cabins" />
       <p>TEST</p>
+      <img
+        alt="cabin-1"
+        src="https://suhwxfapnihtjdetmffz.supabase.co/storage/v1/object/public/cabin-images/cabin_001.jpg"
+      />
     </Row>
   );
 };
--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = 'https://dclaevazetcjjkrzczpc.supabase.co';
+export const supabaseUrl = 'https://suhwxfapnihtjdetmffz.supabase.co';
 
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY';
+const supabaseKey = import.meta.env.VITE_API_KEY_SUPABASE;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
